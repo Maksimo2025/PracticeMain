7b7bc3cd75f214f2bbf77967db3f0a41961292fe
Maksimo2025 <maksimowin@gmail.com>
2025-08-10 14:56:37 -0500

    new added

--- src/components/PageContent/components/PageMain/components/SectionTwo/components/ImageList/index.jsx
+++ src/components/PageContent/components/PageMain/components/SectionTwo/components/ImageList/index.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
-const ImageList = ({ images }) => {
+const styles = {
+  size: {
+    width: " 130px",
+    height: "150px",
+  },
+  btn: {
+    margin: "5px 32px",
+  },
+};
+
+const ImageList = ({ images, remove }) => {
   return (
     <ul>
       {images.map((image) => (
         <li key={image.id}>
-          {/* <img src={image.src} alt={image.alt} /> */}
-          {image.src}
+          <img src={image.src} style={styles.size} alt={image.alt} />
+          <br />
+          <button onClick={() => remove(image.id)} style={styles.btn}>
+            remove
+          </button>
+          <hr />
+          {/* {image.src} */}
         </li>
       ))}
     </ul>
--- src/components/PageContent/components/PageMain/components/SectionTwo/index.jsx
+++ src/components/PageContent/components/PageMain/components/SectionTwo/index.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionWrapper from "../SectionWrapper";
 import ImageList from "./components/ImageList";
 import Column from "./components/Column";
 import Labels from "./components/Labels";
 import predator from "../../../../../../assets/predator.jpg";
 
+const styles = {
+  empty: {
+    margin: "10px 20px",
+    fontSize: "30px",
+    textShadow: "2px 2px 1px red",
+  },
+};
+
 const SectionTwo = () => {
-  const imagesData = [
-    { id: 1, src: <Labels />, alt: "Описание 1" },
-    { id: 2, src: <Labels />, alt: "Описание 2" },
-    { id: 3, src: <Labels />, alt: "Описание 3" },
-  ];
+  const [imagesData, setImagesData] = useState([
+    { id: 1, src: predator, alt: "Описание 1" },
+    { id: 2, src: predator, alt: "Описание 2" },
+    { id: 3, src: predator, alt: "Описание 3" },
+  ]);
+  const removeImage = (id) => {
+    setImagesData(
+      imagesData.filter((imgD) => {
+        return imgD.id !== id;
+      })
+    );
+  };
 
   return (
     <SectionWrapper title="sectionTwo">
       <Column />
-      <ImageList images={imagesData} />
+      {imagesData.length !== 0 ? (
+        <ImageList images={imagesData} remove={removeImage} />
+      ) : (
+        <div style={styles.empty}> Nothing </div>
+      )}
+
       <Labels />
     </SectionWrapper>
   );
